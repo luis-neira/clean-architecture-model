245f389bd11e72ea4ccb6cde5831d2fa6578a15a
Luis Neira <lneira41086@gmail.com>
2021-11-08 22:00:50 -0500

    Rename all products use-cases

--- src/core/use-cases/products/index.js
+++ src/core/use-cases/products/index.js
@@ -1,8 +1,8 @@
 'use strict';
 
 module.exports = {
-  AddProductUseCase: require('./add-product.usecase'),
-  GetProductByIdUseCase: require('./get-product-by-id.usecase'),
-  DeleteProductUseCase: require('./delete-product.usecase'),
-  UpdateProductUseCase: require('./update-product.usecase')
+  AddProductUseCase: require('./add-product.use-case'),
+  GetProductByIdUseCase: require('./get-product-by-id.use-case'),
+  DeleteProductUseCase: require('./delete-product.use-case'),
+  UpdateProductUseCase: require('./update-product.use-case')
 };
